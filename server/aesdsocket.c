a04a5fb813d36b13dbf988c5870adeb861e12492
BoLinTW <k2345777@gmail.com>
2024-02-25 19:31:02 +0800

    fix recv bug

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -14,6 +14,9 @@
 
 #define OUTPUT_FILE "/var/tmp/aesdsocketdata"
 #define PORT        "9000"
+#define BACKLOG     10
+#define BUF_SIZE    1024
+
 
 bool caught_signal = false;
 
@@ -21,7 +24,7 @@ static void signal_handler(int signal_number)
 {
     if(signal_number == SIGINT || signal_number == SIGTERM) {
         caught_signal = true;
-        syslog(LOG_DEBUG, "Caught signal, exiting");
+        syslog(LOG_INFO, "Caught signal, exiting");
         remove(OUTPUT_FILE);
     }
 }
@@ -43,9 +46,14 @@ bool setting_signal()
     return true;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     openlog(NULL, 0, LOG_USER);
+
+    if(!setting_signal()) {
+        closelog();
+        return -1;
+    }
 
     struct addrinfo hints;
     struct addrinfo *servinfo;
@@ -61,82 +69,102 @@ int main()
         return -1;
     }
 
-    int sd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    int sd = socket(PF_INET, SOCK_STREAM, 0);
     if(sd == -1) {
         syslog(LOG_ERR, "socket open failed");
+        closelog();
         return -1;
     }
 
-    if(bind(sd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+    if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) == -1) {
+        syslog(LOG_ERR, "set socket option failed");
+        closelog();
+        return -1;
+    }
+
+    if(bind(sd, servinfo->ai_addr, sizeof(struct sockaddr)) != 0) {
         syslog(LOG_ERR, "bind failed");
         freeaddrinfo(servinfo);
         close(sd);
+        closelog();
         return -1;
     }
     freeaddrinfo(servinfo);
 
-    pid_t pid = fork();
-    if(pid == -1) {
-        syslog(LOG_ERR, "fork failed");
+    pid_t pid;
+    if(argc>1 && strcmp(argv[1], "d")) {
+        switch(pid = fork()) {
+            case -1:
+                syslog(LOG_ERR, "fork failed");
+                close(sd);
+                closelog();
+                return -1;
+
+            case 0:
+                break;
+
+            default:
+                return 0;
+        }
+    }
+
+    if(listen(sd, BACKLOG) != 0) {
+        syslog(LOG_ERR, "listen failed");
+        close(sd);
+        closelog();
         return -1;
     }
-    else if(pid == 0) {
-        if(!setting_signal()) {
+
+    while(!caught_signal) {
+        char data[BUF_SIZE];
+
+        struct sockaddr client;
+        socklen_t client_len = sizeof(struct sockaddr);
+        int sockfd = accept(sd, &client, &client_len);
+        if(sockfd == -1) {
+            syslog(LOG_ERR, "accept failed");
             return -1;
         }
 
-        if(listen(sd, 5) != 0) {
-            syslog(LOG_ERR, "listen failed");
+        struct sockaddr_in *addr_in = (struct sockaddr_in *)&client;
+        char* ip = inet_ntoa(addr_in->sin_addr);
+        syslog(LOG_DEBUG, "Accepted connection from %s", ip);
+
+        int fd = open(OUTPUT_FILE, O_WRONLY | O_APPEND | O_CREAT, 0666);
+        if(fd == -1) {
+            syslog(LOG_ERR, "file open create write failed");
+            close(sd);
+            closelog();
             return -1;
         }
 
-        char data[1024];
-        while(!caught_signal) {
-            struct sockaddr client;
-            socklen_t client_len = sizeof(struct sockaddr);
-            int sockfd = accept(sd, &client, &client_len);
-            if(sockfd == -1) {
-                syslog(LOG_ERR, "accept failed");
-                close(sockfd);
-                return -1;
+        int ret_len;
+        while((ret_len = recv(sockfd, data, BUF_SIZE, 0)) > 0) {
+            write(fd, data, ret_len);
+            if(data[ret_len-1]=='\n') {
+                break;
             }
-
-            struct sockaddr_in *addr_in = (struct sockaddr_in *)&client;
-            char* ip = inet_ntoa(addr_in->sin_addr);
-            syslog(LOG_DEBUG, "Accepted connection from %s", ip);
-
-            int fd = open(OUTPUT_FILE, O_WRONLY | O_APPEND | O_CREAT, 0666);
-            if(fd == -1) {
-                syslog(LOG_ERR, "file open create write failed");
-                return -1;
-            }
-
-            int ret_len;
-            while((ret_len = recv(sockfd, data, sizeof(data), 0)) == sizeof(data)) {
-                write(fd, data, ret_len);
-            }
-
-            if(ret_len > 0) {
-                write(fd, data, ret_len);
-            }
-
-            close(fd);
-
-            fd = open(OUTPUT_FILE, O_RDONLY);
-            if(fd == -1) {
-                syslog(LOG_ERR, "file open read failed");
-                return -1;
-            }
-
-            int read_len;
-            while((read_len = read(fd, data, sizeof(data))) > 0){
-                send(sockfd, data, read_len, 0);
-            }
-            close(fd);
         }
+
+        close(fd);
+
+        fd = open(OUTPUT_FILE, O_RDONLY);
+        if(fd == -1) {
+            syslog(LOG_ERR, "file open read failed");
+            close(sd);
+            closelog();
+            return -1;
+        }
+
+        while((ret_len = read(fd, data, BUF_SIZE)) > 0){
+            send(sockfd, data, ret_len, 0);
+        }
+        close(fd);
+        close(sockfd);
     }
 
     close(sd);
+    closelog();
 
     return 0;
 }
